64b8a8f6c499919d4dc7649ed275318429bd4ea4
Samuel Leite Pereira <131712358+ProgrammerAndInvestor@users.noreply.github.com>
2023-12-03 17:27:24 -0300

    Delete easytask directory

--- easytask/src/pages/login-page.jsx
+++
@@ -1,9 +0,0 @@
-import Login from "../components/login/login";
-
-function LoginPage () {
-    return(
-        <Login />
-    )
-}
-
-export default LoginPage
--- easytask/src/pages/signup-page.jsx
+++
@@ -1,9 +0,0 @@
-import Signup from "../components/singup/singup";
-
-function SignupPage() {
-    return(
-        <Signup/>
-    )
-}
-
-export default SignupPage
